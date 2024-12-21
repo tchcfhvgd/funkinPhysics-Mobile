8e60772ec557cafa2b5c25ddeb944d1bee83dff0
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-12-21 12:13:45 +0800

    Update PsychCamera.hx

--- source/PsychCamera.hx
+++ source/PsychCamera.hx
@@ -1,5 +1,22 @@
 package;
 
+import flixel.sound.FlxSound;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.util.FlxDestroyUtil;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
+
+using StringTools;
+
 // PsychCamera handles followLerp based on elapsed
 // and stops camera from snapping at higher framerates
 
